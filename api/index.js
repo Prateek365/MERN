5ee030a4eaf5c91d6a8341aa12aa7d10f1570189
Prateek Verma <prateekverma365@gmail.com>
2024-02-25 17:16:03 +0530

    created test api route, model, route, controller

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 
 mongoose
@@ -12,8 +13,11 @@ mongoose
     console.log(err);
   })
 
-const app = express();
+const app = express(); 
 
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
+
+
+app.use("/api/user",userRouter)
